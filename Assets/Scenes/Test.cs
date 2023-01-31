28e468b5313d550c38c1b77ec8c12c5338ef2685
snlim00 <snlim00@naver.com>
2023-02-01 02:36:47 +0900

    [의만] 브랜치 테스트

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -1,49 +1,13 @@
-using Mono.Cecil;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using UniRx;
-using UnityEditor;
+using UnityEditor.Tilemaps;
 using UnityEngine;
-using UnityEngine.UI.Extensions;
 
-
-public class Human
+[CreateAssetMenu(fileName = "Test", menuName = "Scriptable Object/Test Data", order = int.MaxValue)]
+public class Test : ScriptableObject
 {
-    public string name;
+    public int a;
+    public float b;
+    public string c;
 
-    public int age;
-    public int height;
-    public int weight;
-
-    public int iq;
-
-    //override : 덮어쓰기
-    public virtual string Introduce()
-    {
-        return "저는 " + name + "입니다.";
-    }
-}
-
-public class Student : Human
-{
-    public string school;
-
-    public override string Introduce()
-    {
-        string prevIntroduce = base.Introduce();
-
-        return prevIntroduce + school + "에 다니고 있습니다.";
-    }
-}
-
-public class Test : MonoBehaviour
-{
-    // main 함수
-    private void Start()
-    {
-        Student uiman = new();
-        uiman.name = "최의만";
-        uiman.school = "한세사이버보안고등학교";
-    }
+    [SerializeField] public List<Vector2> list;
 }
