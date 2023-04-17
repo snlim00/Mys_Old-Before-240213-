d2cf83bb1f5faa61a0b800ba082de00f4d0c8173
snlim00 <100692097+snlim00@users.noreply.github.com>
2023-04-17 16:44:05 +0900

    1. 테스트 캐릭터 이미지 윤하로 변경 2. CharacterIndex 위치 조정 / CharacterPref 크기 조정

--- Assets/Scripts/Dialog/MysObject.cs
+++ Assets/Scripts/Dialog/MysObject.cs
@@ -18,6 +18,6 @@ public class MysObject : MonoBehaviour
 
     public void SetPosition(int position)
     {
-        transform.localPosition = eventMgr.objectPositions[position].transform.localPosition;
+        transform.position = eventMgr.objectPositions[position].transform.position;
     }
 }
