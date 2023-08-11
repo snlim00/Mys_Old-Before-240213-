a83f1b559198b37565cb5e9ddda7c5ce4c2554a3
snlim00 <100692097+snlim00@users.noreply.github.com>
2023-08-11 21:05:24 +0900

    NewScriptManager에 필요한 변수 및 함수 추가

--- Assets/Scripts/NewDialog/NewScriptManager.cs
+++ Assets/Scripts/NewDialog/NewScriptManager.cs
@@ -15,4 +15,101 @@ public class NewScriptManager
     public Dictionary<string, int> requiredStat = new();
 
     public List<ScriptObject> scripts;
+
+
+    public int currentScriptIndex { get; private set; } = 0;
+
+    public ScriptObject currentScript
+    {
+        get
+        {
+            if (currentScriptIndex < scripts.Count)
+            {
+                return scripts[currentScriptIndex];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public ScriptObject nextScript
+    {
+        get
+        {
+            int nextIndex = currentScriptIndex + 1;
+
+            if (nextIndex < scripts.Count)
+            {
+                return scripts[nextIndex];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public ScriptObject prevScript
+    {
+        get
+        {
+            int nextIndex = currentScriptIndex - 1;
+
+            if (nextIndex >= 0)
+            {
+                return scripts[nextIndex];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public ScriptObject firstScript
+    {
+        get
+        {
+            return scripts[0];
+        }
+    }
+
+    public ScriptObject Prev()
+    {
+        currentScriptIndex -= 1;
+
+        return currentScript;
+    }
+
+    public ScriptObject Next()
+    {
+        currentScriptIndex += 1;
+
+        return currentScript;
+    }
+
+    public ScriptObject GotoFirstScript()
+    {
+        currentScriptIndex = 0;
+
+        return currentScript;
+    }
+
+    public ScriptObject SetCurrentScript(int scriptId)
+    {
+        ScriptObject script = scripts.Find(script => script.scriptID == scriptId);
+
+        return SetCurrentScript(script);
+    }
+
+    public ScriptObject SetCurrentScript(ScriptObject script)
+    {
+        int idx = scripts.IndexOf(script);
+
+        currentScriptIndex = idx;
+
+        return currentScript;
+    }
 }
