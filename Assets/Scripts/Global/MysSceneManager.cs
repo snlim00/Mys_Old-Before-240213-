29a8c5863113acd9c92c6dc99baf359ff4a38ae4
snlim00 <100692097+snlim00@users.noreply.github.com>
2023-09-02 19:43:56 +0900

    - 씬 이동 시 페이드 인 / 아웃이 일어나도록 함

--- Assets/Scripts/Global/MysSceneManager.cs
+++ Assets/Scripts/Global/MysSceneManager.cs
@@ -1,35 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using System;
 using UnityEngine.SceneManagement;
 using UniRx;
+using DG.Tweening;
 
 public class MysSceneManager : Singleton<MysSceneManager>
 {
+    private MysSceneManager instance;
+
+    [SerializeField] private Image fadeObj;
+
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
     }
 
+    private void Start()
+    {
+        instance = MysSceneManager.Instance;
+    }
+
     public static void LoadLobbyScene(Action cb)
     {
-        SceneManager.LoadScene("Lobby");
-
-        cb?.Invoke();
+        LoadScene("Lobby", cb);
     }
 
     public static void LoadEditorLobbyScene(Action cb)
     {
-        SceneManager.LoadScene("EditorLobby");
-
-        cb?.Invoke();
+        LoadScene("EditorLobby", cb);
     }
 
     public static void LoadScenarioScene(Action cb, int? scriptGroupId = null)
     {
-        MysSceneManager.LoadScene("ScenarioScene", () => {
+        LoadScene("ScenarioScene", () => {
             cb?.Invoke();
             
             if(scriptGroupId != null)
@@ -42,9 +49,20 @@ public class MysSceneManager : Singleton<MysSceneManager>
 
     public static void LoadScene(string scene, Action cb)
     {
-        SceneManager.LoadScene(scene);
+        Sequence seq = DOTween.Sequence();
 
-        Observable.TimerFrame(1)
-            .Subscribe(_ => cb?.Invoke());
+        seq.Append(Instance.fadeObj.DOFade(1, 1).SetEase(Ease.OutQuad));
+
+        seq.AppendCallback(() =>
+        {
+            SceneManager.LoadScene(scene);
+
+            Observable.TimerFrame(1)
+                .Subscribe(_ => cb?.Invoke());
+        });
+
+        seq.Append(Instance.fadeObj.DOFade(0, 1).SetEase(Ease.OutQuad));
+
+        seq.Play();
     }
 }
